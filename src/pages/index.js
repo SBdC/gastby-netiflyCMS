1b09410334c3a1e347683b6bff4e2d7d38a2efd6
SBdC <denominatu@hotmail.com>
2019-11-02 18:42:05 +0100

    added thumb

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -21,6 +20,7 @@ class BlogIndex extends React.Component {
           return (
             <article key={node.fields.slug}>
               <header>
+    
                 <h3
                   style={{
                     marginBottom: rhythm(1 / 4),
@@ -34,7 +34,8 @@ class BlogIndex extends React.Component {
                 
               </header>
               <section>
-                <p
+               
+                <div style={{ marginBottom: rhythm(1) }}
                   dangerouslySetInnerHTML={{
                     __html: node.frontmatter.description || node.excerpt,
                   }}
@@ -49,6 +50,8 @@ class BlogIndex extends React.Component {
 }
 
 export default BlogIndex
+
+
 
 export const pageQuery = graphql`
   query {
